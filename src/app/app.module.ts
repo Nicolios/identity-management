1c2b698ffa2bb53d5d976150f0c6983ff4f33c4b
Nicolas De Jesus <ndejesus34800@gmail.com>
2021-10-26 16:45:18 +0200

    add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {MatTableModule} from "@angular/material/table";
 import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -36,7 +39,10 @@ import {MatPaginatorModule} from "@angular/material/paginator";
     MatIconModule,
     MatListModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
