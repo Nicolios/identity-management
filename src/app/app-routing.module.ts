c9ef8ecaeb41c81956f2eb8b461bf761cf41dd4d
Nicolas De Jesus <ndejesus34800@gmail.com>
2021-10-31 15:55:05 +0100

    modify routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import {LdapListComponent} from "./ldap-management/ldap-list/ldap-list.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {LdapEditComponent} from "./ldap-management/ldap-edit/ldap-edit.component";
-import {LdapAddComponent} from "./ldap-management/ldap-add/ldap-add.component";
 
 const routes: Routes = [
-  {path: 'users/list', component: LdapListComponent},
-  {path: 'user/add', component: LdapAddComponent},
-  {path: 'user/:id', component: LdapEditComponent},
   {path: '**', component:PageNotFoundComponent}
 ];
 
