67721efbb2fe7676926dbda1860f8e5a45a52f8b
Nicolas De Jesus <ndejesus34800@gmail.com>
2021-10-31 15:46:11 +0100

    move to ldap-management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LdapListComponent } from './ldap-list/ldap-list.component';
+import { LdapListComponent } from './ldap-management/ldap-list/ldap-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app-material.module';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './ldap-management/navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -19,11 +19,11 @@ import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 import {MatInputModule} from "@angular/material/input";
-import { LdapDetailComponent } from './ldap-detail/ldap-detail.component';
+import { LdapDetailComponent } from './ldap-management/ldap-detail/ldap-detail.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import { LdapEditComponent } from './ldap-edit/ldap-edit.component';
-import { LdapAddComponent } from './ldap-add/ldap-add.component';
+import { LdapEditComponent } from './ldap-management/ldap-edit/ldap-edit.component';
+import { LdapAddComponent } from './ldap-management/ldap-add/ldap-add.component';
 import { AlertComponent } from './share/alert/alert.component';
 import { LdapManagementModule } from './ldap-management/ldap-management.module';
 
